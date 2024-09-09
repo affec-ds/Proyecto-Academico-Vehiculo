1864a9a17eaa0aa509b7e1f03a870e00255a6736
affec-ds <affectus.contact@gmail.com>
2024-09-09 13:06:59 -0300

    Añadidos archivos de configuración, datos y notebooks para el proyecto

---
+++ notebooks/app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import streamlit as st
+import plotly_express as px
+
+df = pd.read_csv(
+    'C:/Users/affec/OneDrive/Escritorio/Proyecto Sprint 6 Vehiculos/proyecto_vehiculo/vehicles_us.csv')
+df.head()
